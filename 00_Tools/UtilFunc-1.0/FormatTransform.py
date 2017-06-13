20225f8a087fe1fc156b20a23f37b48b112732a6
Kin Wah Edward Lin <EdwardLin2014@users.noreply.github.com>
2017-06-13 11:56:30 +0800

    Add files via upload

--- 00_Tools/UtilFunc-1.0/FormatTransform.py
+++ 00_Tools/UtilFunc-1.0/FormatTransform.py
@@ -16,5 +16,19 @@ def prepareSineSynth( X, ploc, Parm ):
 
     return cX
 
+def PartialsToBinaryMask( Partials, Parm ):
+    '''
+    %% Transform Partials into a binary mask
+    %   Partials: struct type
+    %   Parm: System configuration
+    %   return ploc: peak location as a numBins*numFrames matrix
+    '''
+    ploc = np.zeros((Parm.numBins, Parm.numFrames))
 
-
+    numPartials = len(Partials)
+    for i in np.arange(numPartials):
+        Partial = Partials[i]
+        for j in np.arange(Partial.size):
+            ploc[Partial.freqIdx[j],Partial.period[0]+(j-1)] = 1
+    
+    return ploc
